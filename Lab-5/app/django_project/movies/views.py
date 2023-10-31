e7dfb88aa4b2b2e6bb4e4a4296d206e66fe741db
Daniela-Briceno <daniela.bricenop@userena.cl>
2023-10-30 21:36:51 -0300

    Agregando peliculas de ejemplo 'sport' Version 2.0

--- Lab-5/app/django_project/movies/views.py
+++ Lab-5/app/django_project/movies/views.py
@@ -7,6 +7,8 @@ from .models import Gender, Movies
 def index(request):
     return render(request, 'index.html')
 
+# SE DEBE UNIR LA FUNCION LISTA_GENEROS CON LISTA_MOVIES
+
 def lista_generos(request):
     data = getGender()
     print(data)
@@ -34,24 +36,31 @@ def getGender():
         return data.get("response", [])  # Devuelve la lista de generos
     else:
         return []  # Devuelve una lista vacía en caso de error
-'''
+
 def lista_movies(request):
     data = getMovies()
     peliculas = []
 
     for pelicula in data:
+        nombre = pelicula.get('title', 'Nombre no disponible')
+        resumen = pelicula.get('description', 'Descripción no disponible')
+        
+        poster = pelicula.get('poster', {})
+        imagen = poster.get('url', 'URL no disponible')
+        
         pelicula_procesada = {
-            'nombre': pelicula['name'],
-            'resumen': pelicula['description'],
-            'imagen': pelicula['poster'],
-            'genero': pelicula['id'],
+            'nombre': nombre,
+            'resumen': resumen,
+            'imagen': imagen,
         }
         peliculas.append(pelicula_procesada)
     print(peliculas[0])
     return render(request, 'index.html', {'movies': peliculas})
     
 def getMovies():
-    url = "https://api.filmon.com/api/vod/search?"
+    # AQUÍ, SE DEBE RECIBIR POR PARÁMERO EL GENERO
+    # ASÍ PODER REMPLAZAR EL VALOR DEL GENERO EN EL URL Y VOLVERLO DINAMICO
+    url = "http://api.filmon.com/api/vod/search?genre=sports"
     headers = {
         "accept": "application/json",
         "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MWJjN2I0N2I4OWM1MDdhZWUwYjJmMjliOGEyMmU4NSIsInN1YiI6IjY1MzlhM2RmOTU1YzY1MDExYmUwYzhkZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cuKI41__m1E2SBGXAf3qGLVHq9vqnmcyTAYrO_11MwU"
@@ -60,8 +69,7 @@ def getMovies():
     
     if response.status_code == 200:
         data = response.json()
-        return data.get("results", [])  # Devuelve la lista de películas
+        return data.get("response", [])
     else:
-        return []  # Devuelve una lista vacía en caso de error
+        return []  
 
-'''
